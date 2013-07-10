19d95a1e22887e23ee5128b808f02504572ba62b
Margit McGrath <margitmc@gmail.com>
2013-07-10 05:59:24 +0000

    Now uses index.html update 10

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html').toString);
+  var buff = new Buffer(fs.readFileSync('index.html'));
+  response.send(buff.toString);
 });
 
 var port = process.env.PORT || 5000;
